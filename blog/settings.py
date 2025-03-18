eded29c8b9e8e75799712c19a3d9b6284e04e884
resumeproject <killerhyper89@gmail.com>
2025-03-18 12:15:37 +0530

    Fixed STATIC_ROOT issue

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^8vmymh*_4lf1in7==d_%%!c&@w0cogdhuzu%x)c0*ypv(gtm^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,11 +118,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = "/static/"
+
+# Add this line to specify the static root directory
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Ensure STATICFILES_DIRS is set correctly (optional, for additional static files)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    
+    os.path.join(BASE_DIR, "static"),  # Adjust if needed
 ]
 
 
